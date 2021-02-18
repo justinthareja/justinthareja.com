96c9f452ab1c7344454aff2066665d896d6a172e
Justin Thareja <justin.thareja@gmail.com>
2021-02-17 23:13:00 -0500

    added changeTheme function

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("what up")
+var config = {
+    "frost": "./assets/frost.mp4",
+    "earth": "./assets/earth.mp4"
+};
+
+var state = {
+    theme: "frost"
+};
+
+var $video = document.querySelector(".js-bg");
+var $source = $video.querySelector(".js-src");
+
+function setState(newState) {
+    state = {
+        ...state,
+        ...newState
+    };
+}
+
+function changeTheme(newTheme) {
+    document.body.classList.remove(state.theme);
+    setState({ theme: newTheme });
+    document.body.classList.add(newTheme);
+
+    const src = config[newTheme];
+
+    $source.setAttribute("src", src);
+    $video.load();
+    $video.play();
+}
+
