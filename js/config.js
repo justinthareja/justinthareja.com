74e6053fcc2ffdd8eeadacddca826471b6c471b3
Justin Thareja <justin.thareja@gmail.com>
2021-02-23 13:14:02 -0500

    create js folder

---
+++ js/config.js
@@ -0,0 +1,43 @@
+var config = {
+    frost: {
+        id: 1,
+        name: "frost",
+        video: {
+            mp4: "./assets/frost/knockout.mp4",
+            webm: "./assets/frost/knockout.webm",
+            poster: "./assets/frost/knockout-poster.png"
+        },
+        wardrobe: {
+            primary: "./assets/frost/goggles.svg",
+            secondary: "./assets/frost/beanie.svg",
+        }
+    },
+
+    earth: {
+        id: 2,
+        name: "earth",
+        video: {
+            mp4: "./assets/earth/knockout.mp4",
+            webm: "./assets/earth/knockout.webm",
+            poster: "./assets/earth/knockout-poster.png",
+        },
+        wardrobe: {
+            primary: "./assets/earth/helmet.svg",
+            secondary: "./assets/earth/anchor.svg"
+        }
+    },
+
+    wind: {
+        id: 3,
+        name: "wind",
+        video: {
+            mp4: "./assets/wind/knockout.mp4",
+            webm: "./assets/wind/knockout.webm",
+            poster: "./assets/wind/knockout-poster.png"
+        },
+        wardrobe: {
+            primary: "./assets/wind/sunglasses.svg",
+            secondary: "./assets/wind/sunblock.svg"
+        }
+    }
+};
